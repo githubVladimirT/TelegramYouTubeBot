602dea849a9790949094b1f5f7876d9f63b82b6e
githubVladimirT <vladimir.tolstov@selfip.ru>
2021-10-31 11:48:13 +0300

    Change the comments

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ import (
 )
 
 func main() {
-	botToken := "2096039520:AAGXsByirzENLmK1i-7X3CQnvfwCzntgJno" // Token of telegram bot
-	botApi := "https://api.telegram.org/bot" // Bot api
-	botUrl := botApi + botToken // Bot Url = botApi + botToken
+	botToken := "2096039520:AAGXsByirzENLmK1i-7X3CQnvfwCzntgJno" // Token of telegram bot@githubVladimirT
+	botApi := "https://api.telegram.org/bot" // Bot api@githubVladimirT
+	botUrl := botApi + botToken // Bot Url = botApi + botToken@githubVladimirT
 	offset := 0
 
 	for {
